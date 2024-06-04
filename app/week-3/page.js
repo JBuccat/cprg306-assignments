8ec535c36ff2dfc5b5fb86fa7a963a592eb8e162
JBuccat <jonathan.buccat@edu.sait.ca>
2024-06-03 22:48:30 -0600

    week-3
    Shopping List Assignment

---
+++ app/week-3/page.js
@@ -0,0 +1,13 @@
+import ItemList from "./item-list";
+
+
+
+
+export default function Page(){
+    return(
+        <main className="p-8 bg-gray-900 min-h-screen">
+            <h1 className="text-4xl font-bold mb-6 text-white">Shopping List</h1>
+            <ItemList />
+        </main>
+    );
+}
